42dec1510e00d179ccfee7517526d43222599883
João Vitor <7700161@aluno.faculdadecotemig.com.br>
2023-10-23 22:10:53 -0300

    back-end: implementando verificação em duas etapas

--- lib/services/auth_request.dart
+++ lib/services/auth_request.dart
@@ -2,15 +2,16 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:retake_app/services/auth_cookies.dart';
+import 'package:retake_app/services/multi_factor_authentication.dart';
 
 class AuthRequestButton extends StatefulWidget {
   const AuthRequestButton({Key? key}) : super(key: key);
 
   @override
-  _AuthRequestButtonState createState() => _AuthRequestButtonState();
+  _AuthRequest createState() => _AuthRequest();
 }
 
-class _AuthRequestButtonState extends State<AuthRequestButton> {
+class _AuthRequest extends State<AuthRequestButton> {
   String resultText = '';
   bool isLoading = false;
   late TextEditingController usernameController;
@@ -24,6 +25,7 @@ class _AuthRequestButtonState extends State<AuthRequestButton> {
   }
 
   void onPressed() async {
+    final navigator = Navigator.of(context);
     setState(() {
       isLoading = true;
     });
@@ -37,6 +39,11 @@ class _AuthRequestButtonState extends State<AuthRequestButton> {
       isLoading = false;
       resultText = result;
     });
+
+    if (verifyResponse(result)) {
+      navigator.pushReplacement(MaterialPageRoute(
+          builder: (context) => const MultiFactorAuthButton()));
+    }
   }
 
   @override
@@ -62,7 +69,7 @@ class _AuthRequestButtonState extends State<AuthRequestButton> {
     );
   }
 
-  Future<String> auth(String username, String password) async {
+  Future<String> auth(String userName, String password) async {
     final authCookies = AuthCookies();
     final cookies = await authCookies.cookiesAuth();
     final url = Uri.parse('https://auth.riotgames.com/api/v1/authorization');
@@ -74,7 +81,7 @@ class _AuthRequestButtonState extends State<AuthRequestButton> {
 
     final body = {
       "type": "auth",
-      "username": username,
+      "username": userName,
       "password": password,
       "remember": true,
       "language": "pt_BR",
@@ -95,5 +102,9 @@ class _AuthRequestButtonState extends State<AuthRequestButton> {
     } catch (e) {
       return '$e';
     }
+  }
+
+  bool verifyResponse(String response) {
+    return response.contains("multifactor") ? true : false;
   }
 }
--- lib/services/multi_factor_authentication.dart
+++ lib/services/multi_factor_authentication.dart
@@ -1,6 +1,89 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class MultiFactorAuth {
-  mfaAuth() {}
+import 'package:retake_app/services/connector.dart';
+
+class MultiFactorAuthButton extends StatefulWidget {
+  const MultiFactorAuthButton({Key? key}) : super(key: key);
+
+  @override
+  _MultiFactorAuth createState() => _MultiFactorAuth();
+}
+
+class _MultiFactorAuth extends State<MultiFactorAuthButton> {
+  String resultText = '';
+  bool isLoading = false;
+  late TextEditingController authCodeController;
+
+  @override
+  void initState() {
+    super.initState();
+    authCodeController = TextEditingController();
+  }
+
+  void onPressed() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    final result = await authConnector(
+      authCodeController.text,
+    );
+
+    setState(() {
+      isLoading = false;
+      resultText = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Autenticação de duas etapas'),
+        ),
+        body: Column(
+          children: [
+            TextField(
+              controller: authCodeController,
+              decoration:
+                  const InputDecoration(labelText: 'Código de autenticação'),
+            ),
+            ElevatedButton(
+                onPressed: isLoading ? null : onPressed,
+                child: const Text('Realizar autenticação')),
+            const SizedBox(height: 16),
+            if (isLoading)
+              const CircularProgressIndicator()
+            else
+              Text(resultText)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<String> authConnector(String code) async {
+    final url = Uri.parse('https://auth.riotgames.com/api/v1/authorization');
+
+    final Map<String, String> headers = {
+      "Content-Type": "application/json",
+    };
+
+    final body = {"type": "multifactor", "code": code, "remenberDevice": true};
+
+    try {
+      final response =
+          await http.put(url, headers: headers, body: jsonEncode(body));
+      if (response.statusCode == 200) {
+        return 'Auententicação feita com sucesso!';
+      } else {
+        return '${response.statusCode} \n ${response.body}';
+      }
+    } catch (e) {
+      return 'Houve algum erro, tente novamente \n $e';
+    }
+  }
 }
