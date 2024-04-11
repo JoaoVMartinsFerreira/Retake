0ab17329e9df2b50df13ef4c1db8641b16226c56
yTekilla <71700161@luno.faculdadecotemig.br>
2024-04-11 15:15:16 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:retake_app/auth/auth_request.dart';
-import 'package:retake_app/custom%20widgets/footer_menu_bar.dart';
-import 'package:retake_app/menu/main_menu.dart';
-import 'package:retake_app/party%20endpoints/start_queue_game.dart';
+
 
 void main() {
   runApp(const MyApp());
